3153a3d6ed45d36c037a9a32ed05aefa1337b597
Tej Bahadur Parmar <49992418+Tejpa@users.noreply.github.com>
2020-04-04 23:16:10 +0530

    CURD_php

---
+++ conn.php
@@ -0,0 +1,15 @@
+<?php
+   $conn = mysqli_connect('localhost','root');
+   mysqli_select_db($conn,'crud');
+   
+  /*  if($conn)
+   {
+	   echo "connetcted";
+   }
+   else
+   {
+	   echo "Not connected";
+   } */
+
+ 
+?>
